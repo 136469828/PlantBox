1dd4df2dfa524f368adc16d57e80142c72eb8479
136469828 <136469828@qq.com>
2016-04-27 08:57:07 +0800

    Initial Commit

---
+++ PlantBox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlantBox
+//
+//  Created by admin on 16/4/27.
+//  Copyright © 2016年 JCK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PlantBox/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PlantBox
+//
+//  Created by admin on 16/4/27.
+//  Copyright © 2016年 JCK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
